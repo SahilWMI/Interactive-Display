f7a7d59caf53142fb74543afb292292129ce8322
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-09-03 22:10:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let distMouse = 50;
 let b;
 let size = 50;
+
+function setup() {
+    createCanvas(400, 400);
+    rectMode(CENTER);
+    angleMode(DEGREES);
+
+    b = new CSSLayerBlockRule(width/2, height/2);
+}
